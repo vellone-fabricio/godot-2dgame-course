fc4f7045dd578169b98907d98f16101fc9d182be
fvellone <fabriciovellone@gmail.com>
2024-06-26 22:17:46 -0400

    added upgrade managers and features

--- scenes/manager/experience_manager.gd
+++ scenes/manager/experience_manager.gd
@@ -1,14 +1,27 @@
 extends Node
 
+signal experience_updated(current_exp: float ,target_experience: float)
+signal level_up(new_level: int)
+
+const TARGET_EXPERIENCE_GROWTH = 5
+
 var current_exp = 0
+var current_level = 1
+var target_experience = 5
 
 func _ready():
 	GameEvents.experience_vial_collected.connect(on_experience_vial_collected)
 	
 
 func increment_experience(number: float):
-	current_exp += number
-	print(current_exp)
+	current_exp = min(current_exp + number, target_experience)
+	experience_updated.emit(current_exp, target_experience)
+	if current_exp == target_experience:
+		current_level += 1
+		target_experience += TARGET_EXPERIENCE_GROWTH
+		current_exp = 0
+		experience_updated.emit(current_exp, target_experience)
+		level_up.emit(current_level)
 
 func on_experience_vial_collected(number: float):
 	increment_experience(number)
