e6effcce1f35e83a83d7dc164e7dc1d086c491ea
fvellone <fabriciovellone@gmail.com>
2024-06-28 11:35:01 -0400

    add health bar and damage

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -3,12 +3,19 @@ extends CharacterBody2D
 const MAX_SPEED = 125
 const ACC_SMOOTHING = 25
 
-# Called when the node enters the scene tree for the first time.
+@onready var damage_interval_timer = $DamageIntervalTimer
+@onready var health_component = $HealthComponent
+@onready var health_bar = $HealthBar
+
+var number_colliding_bodies = 0
+
 func _ready():
-	pass # Replace with function body.
+	$CollisionArea2D.body_entered.connect(on_body_entered)
+	$CollisionArea2D.body_exited.connect(on_body_exited)
+	damage_interval_timer.timeout.connect(on_damage_timeout_interval)
+	health_component.health_changed.connect(on_health_changed)
+	updated_health_display()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var mov_vector = get_movement_vector()
 	var direction = mov_vector.normalized()
@@ -23,3 +30,26 @@ func get_movement_vector():
 	var y_mov = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	
 	return Vector2(x_mov, y_mov)
+
+func check_deal_damage():
+	if number_colliding_bodies == 0 || !damage_interval_timer.is_stopped():
+		return
+	
+	health_component.damage(1)
+	damage_interval_timer.start()
+
+func updated_health_display():
+	health_bar.value = health_component.get_health_percentage()
+
+func on_body_entered(other_body: Node2D):
+	number_colliding_bodies += 1
+	check_deal_damage()
+
+func on_body_exited(other_body: Node2D):
+	number_colliding_bodies = max(0, number_colliding_bodies - 1)
+
+func on_damage_timeout_interval():
+	check_deal_damage()
+
+func on_health_changed():
+	updated_health_display()
