155f0ba700e13994a9ba7c6d17b1bc983ecfb95e
fvellone <fabriciovellone@gmail.com>
2024-07-12 20:35:35 -0400

    add main menu + options menu

---
+++ scenes/ui/main_menu.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+var options_scene = preload("res://scenes/ui/options_menu.tscn")
+
+func _ready():
+	%PlayButton.pressed.connect(on_play_button)
+	%OptionsButton.pressed.connect(on_options_button)
+	%QuitButton.pressed.connect(on_quit_button)
+
+func on_play_button():
+	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
+
+
+func on_options_button():
+	var options_instance = options_scene.instantiate()
+	add_child(options_instance)
+	options_instance.back_pressed.connect(on_back_pressed.bind(options_instance))
+
+
+func on_quit_button():
+	get_tree().quit()
+
+
+func on_back_pressed(options_instance: Node):
+	options_instance.queue_free()
