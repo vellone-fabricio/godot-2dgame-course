0de24a5faf05e363e6b662b07b8efbc96365bc26
fvellone <fabriciovellone@gmail.com>
2024-07-22 19:23:03 -0400

    update meta card and create meta menu

--- scenes/ui/meta_upgrade_card.gd
+++ scenes/ui/meta_upgrade_card.gd
@@ -2,9 +2,14 @@ extends PanelContainer
 
 @onready var name_label: Label = $%NameLabel
 @onready var description_label: Label = $%DescriptionLabel
+@onready var progress_bar = %ProgressBar
+@onready var purchase_button = %PurchaseButton
+@onready var progress_label = %ProgressLabel
+
+var upgrade: MetaUpgrade
 
 func _ready():
-	gui_input.connect(on_gui_input)
+	purchase_button.pressed.connect(on_purchase_pressed)
 
 
 func select_card():
@@ -12,9 +17,27 @@ func select_card():
 
 
 func set_meta_upgrade(upgrade: MetaUpgrade):
-	name_label.text = upgrade.name
+	self.upgrade = upgrade
+	name_label.text = upgrade.title
 	description_label.text = upgrade.description
+	update_progress()
 
-func on_gui_input(event: InputEvent):
-	if event.is_action_pressed("left_click"):
-		select_card()
+
+func update_progress():
+	var meta_upgrade_currency = MetaProgression.save_data["meta_upgrade_currency"]
+	var percent = MetaProgression.save_data["meta_upgrade_currency"] / upgrade.experience_cost
+	percent = min(percent, 1)
+	progress_bar.value = percent
+	purchase_button.disabled = percent < 1
+	progress_label.text = str(meta_upgrade_currency) + "/" + str(upgrade.experience_cost)
+
+
+func on_purchase_pressed():
+	if upgrade == null:
+		return
+	
+	MetaProgression.add_meta_upgrade(upgrade)
+	MetaProgression.save_data["meta_upgrade_currency"] -= upgrade.experience_cost
+	MetaProgression.save()
+	get_tree().call_group("meta_upgrade_card", "update_progress")
+	select_card()
