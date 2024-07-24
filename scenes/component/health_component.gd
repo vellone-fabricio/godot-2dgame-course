7758c2903f928c83f702a5ab9905ce1742b84a14
fvellone <fabriciovellone@gmail.com>
2024-07-23 20:17:42 -0400

    add meta health regen

--- scenes/component/health_component.gd
+++ scenes/component/health_component.gd
@@ -3,6 +3,7 @@ class_name HealthComponent
 
 signal died
 signal health_changed
+signal health_decreased
 
 @export var max_health: float = 10
 var current_health
@@ -11,9 +12,15 @@ func _ready():
 	current_health = max_health
 
 func damage(damage_value: float):
-	current_health = max(0, current_health - damage_value)
+	current_health = clamp(current_health - damage_value, 0, max_health)
 	health_changed.emit()
+	if damage_value > 0:
+		health_decreased.emit()
 	Callable(check_death).call_deferred()
+
+func heal(value: int):
+	health_changed.emit()
+	damage(-value)
 
 func get_health_percentage():
 	if max_health <= 0:
--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@export var arena_time_manager: Node
+
 @onready var damage_interval_timer = $DamageIntervalTimer
 @onready var health_component = $HealthComponent
 @onready var health_bar = $HealthBar
@@ -12,11 +14,13 @@ var number_colliding_bodies = 0
 var base_speed = 0
 
 func _ready():
+	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
 	base_speed = velocity_component.max_speed
 	
 	$CollisionArea2D.body_entered.connect(on_body_entered)
 	$CollisionArea2D.body_exited.connect(on_body_exited)
 	damage_interval_timer.timeout.connect(on_damage_timeout_interval)
+	health_component.health_decreased.connect(on_health_decreased)
 	health_component.health_changed.connect(on_health_changed)
 	GameEvents.ability_upgrade_added.connect(on_ability_upgrade_added)
 	updated_health_display()
@@ -64,11 +68,13 @@ func on_body_exited(other_body: Node2D):
 func on_damage_timeout_interval():
 	check_deal_damage()
 
-func on_health_changed():
+func on_health_decreased():
 	GameEvents.emit_player_damaged()
-	updated_health_display()
 	$RandomStreamPlayerComponent.play_random()
 
+
+func on_health_changed():
+	updated_health_display()
 
 func on_ability_upgrade_added(ability_upgrade: AbilityUpgrade, current_upgrades: Dictionary):
 	if ability_upgrade is Ability:
@@ -76,3 +82,11 @@ func on_ability_upgrade_added(ability_upgrade: AbilityUpgrade, current_upgrades:
 		abilities.add_child(ability.ability_controller_scene.instantiate())
 	elif ability_upgrade.id == "player_speed":
 		velocity_component.max_speed = base_speed + (base_speed * current_upgrades["player_speed"]["quantity"] * .1) 
+
+
+func on_arena_difficulty_increased(difficulty: int):
+	var health_regen_quantity = MetaProgression.get_upgrade_count("health_regeneration")
+	if health_regen_quantity > 0:
+		var is_thirty_second_interval = (difficulty % 6) == 0
+		if is_thirty_second_interval:
+			health_component.heal(health_regen_quantity)
